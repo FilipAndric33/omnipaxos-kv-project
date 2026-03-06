71cf4102bd3b566115d499ea58eb646b242b43fe
Filip Andric <andric3filip3@gmail.com>
2026-03-06 17:13:40 +0100

    removed inner git repo from the crates folder.

---
+++ crates/custom-paxos/omnipaxos_macros/tests/derive_test.rs
@@ -0,0 +1,28 @@
+#[test]
+fn build_op_test() {
+    use omnipaxos::{macros::Entry, ClusterConfig, OmniPaxos, OmniPaxosConfig, ServerConfig};
+    use omnipaxos_storage::memory_storage::MemoryStorage;
+
+    #[derive(Clone, Debug, Entry)]
+    struct TestEntry {
+        pub _field1: u64,
+        pub _field2: String,
+    }
+
+    let cluster_config = ClusterConfig {
+        configuration_id: 1,
+        nodes: vec![1, 2, 3],
+        ..Default::default()
+    };
+    let server_config = ServerConfig {
+        pid: 1,
+        ..Default::default()
+    };
+    let config = OmniPaxosConfig {
+        cluster_config,
+        server_config,
+    };
+
+    let _omnipaxos: OmniPaxos<TestEntry, MemoryStorage<TestEntry>> =
+        config.build(MemoryStorage::default()).unwrap();
+}
